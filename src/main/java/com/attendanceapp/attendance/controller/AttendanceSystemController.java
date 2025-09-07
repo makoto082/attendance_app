41a20818064bea50ee2ff26cab612680d2227e98
m_makoto <m.makoto9922@gmail.com>
2025-09-07 23:47:01 +0900

    WIP:view

--- src/main/java/com/attendanceapp/attendance/controller/AttendanceSystemController.java
+++ src/main/java/com/attendanceapp/attendance/controller/AttendanceSystemController.java
@@ -1,10 +1,27 @@
 package com.attendanceapp.attendance.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AttendanceSystemController {
 	/** DI */
 	
+	// 勤怠登録画面表示
+	@GetMapping("/")
+	public String attendanceView() {
+		return "main";
+	}
 	
+	// 管理画面表示
+	@GetMapping("/maintenance")
+	public String maintenanceView() {
+		return "maintenance";
+	}
+	
+	// 管理者用画面表示
+	@GetMapping("/admin")
+	public String adminView() {
+		return "admin";
+	}
 }
